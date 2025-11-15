f1ddd11fe1988968cf94ab4addd13f11c7f7f8ac
Fachry <151fachry@gmail.com>
2025-11-15 22:04:10 +0700

    insial commit

---
+++ hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("Welcome to Go programming.", 123)
+	fmt.Println("True or False?", true)
+}
---
+++ string.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// func main() {
+// 	// to get the length of a string
+// 	fmt.Println(len("this string"))
+// 	// to get a specific character in a string (byte value)
+// 	fmt.Println("this string"[2])
+// }
+
+func main() {
+	var name string
+
+	name = "Fachry"
+	fmt.Println(name)
+	name = "Fachry Alifian"
+	fmt.Println(name)
+}
